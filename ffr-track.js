7f0fe6ba4b8173f6527c357171bb1c3e75905aba
KottaiRajAit <119566130+KottaiRajAit@users.noreply.github.com>
2024-10-03 18:06:39 +0530

    Update ffr-track.js

--- ffr-track.js
+++ ffr-track.js
@@ -1,44 +1,12 @@
-let loc = window.location.pathname;
-let ochnkey = ''
-let crmDomain = ''
-if (window.location.origin.includes('staging') || window.location.origin.includes("localhost")) {
-    ochnkey = 'sw1cs4S4SCdL8fN85dC5PuDU';
-    crmDomain = "http://localhost:3000"
-} else {
-     ochnkey = 'sw1cs4S4SCdL8fN85dC5PuDU';
-    crmDomain = "https://46ab-14-102-2-74.ngrok-free.app"
-}
-function getCurrentPosition() {
-  console.log("inside the coords1111")
-   var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-  var thisInst = this;
-  let lat = thisInst.readCookie("ochncoords_latitude");
-  navigator.geolocation.getCurrentPosition(function(pos) {
-      var crd = pos.coords;
-      thisInst.createCookie("ochncoords_latitude", crd.latitude, 10);
-      thisInst.createCookie("ochncoords_longitude", crd.longitude, 10);
-      if (!lat || lat != crd.latitude) {
-          thisInst.init("", true);
-      }
-      return crd.latitude + "," + crd.longitude;
-  }, function(){ return ""; }, options);
-}
-getCurrentPosition()
-function getCookie(cookieName) {
-    const cookies = document.cookie.split(';');
-    for (let index = 0; index < cookies.length; index++) {
-        const cookie = cookies[index].trim();
-        // Check if this cookie starts with the desired name
-        if (cookie.startsWith(`${cookieName}=`)) {
-            return cookie.substring(cookieName.length + 1);
-        }
-    }
-    return null;
-}
+
+/**
+ * Set the API key
+ * @param {string} ochnkey - The API key
+ */
+
+let ochnkey = 'jtxkkvu39rkc3wuy4s6xns1z6';
+var enableLogging = true;
+
 (function (d, s, id) {
     let js, ojs = d.getElementsByTagName(s)[0];
 
@@ -46,66 +14,21 @@ function getCookie(cookieName) {
 
     js = d.createElement(s); js.id = id; js.async = !0;
 
-    js.src = crmDomain + "/jsapi/omnichannel.js";
-    let consentCookie = getCookie('cookie_consent');
-    let cookieAccepted = false
-    if(consentCookie === 'declined'){ 
-      console.log('declined')
-    }
-    else if(consentCookie === 'accepted'){
-      cookieAccepted = true
-    }
-    else{
-      consentCookie = consentCookie ? JSON.parse(consentCookie) : null;
-      cookieAccepted = consentCookie?.analytics
-    }
-    // if (cookieAccepted) {
-        ojs.parentNode.insertBefore(js, ojs);
-    // }    
+    js.src = "https://staging.footprintsforretail.com/jsapi/omnichannel.js";
+
+    // js.src = "http://localhost:3000/jsapi/omnichannel.js";
+
+    ojs.parentNode.insertBefore(js, ojs);
 
 }(document, 'script', 'omnichanneltrack'));
 
-function ochntrack() { //initialization function
-    let consentCookie = getCookie('cookie_consent');
-    let cookieAccepted = false
-    if(consentCookie === 'declined'){ 
-        console.log('declined')
-    }
-    else if(consentCookie === 'accepted'){
-      cookieAccepted = true
-    }
-    else{
-      consentCookie = consentCookie ? JSON.parse(consentCookie) : null;
-      cookieAccepted = consentCookie?.marketing
-    }
-    // if (cookieAccepted) {
-        if (ochn && ochn.init) ochn.init();
-        if (ochn && ochn.send) ochn.send("action", "visit");
-    // }
-}
-ochntrack()
+/**
+ * Initialization function 
+ * ocnh.init()   - Initialized the F-AI omnichannel Js library
+ * ochn.send()   - Tracking the search of the page
+ */
 
-
-window.onclick = () => {
-  console.log("vvvvvvvvvv")
-    // if (window.location.pathname != loc) {
-        loc = window.location.pathname;
-        let consentCookie = getCookie('cookie_consent');
-        let cookieAccepted = false;
-        if(consentCookie === 'declined'){ 
-          console.log('declined')
-        } else if(consentCookie === 'accepted'){
-          cookieAccepted = true
-        } else{
-          consentCookie = consentCookie ? JSON.parse(consentCookie) : null;
-          cookieAccepted = consentCookie?.analytics
-        }
-        console.log("eeeeeeeeeee")
-        // if (cookieAccepted) {
-          if (ochn && ochn.send){
-            console.log("vvvvvvvvvbbbbbb",ochn.send)
-            ochn.send("action", "visit")
-          } 
-        // }
-    // }
+async function ochntrack() {
+    if (ochn.init) ochn.init();
+    if (ochn.send) ochn.send("action", "visit");
 }
